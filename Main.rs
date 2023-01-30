fc44a194de9f535d3de5d8446e402f921e03aa28
Levi <levimogford@gmail.com>
2023-01-30 12:04:38 -0600

    Update Main.rs

--- Main.rs
+++ Main.rs
@@ -1,3 +1,15 @@
 fn main(){
   println!("levi wuz here");
 }
+
+//fn set_input_source(txt:String){
+//fn set_output_source(txt:String){
+
+/*
+ * file_loc = the path to the file location
+ * input = whether this is the var input or the value outcome output file
+ * ofc true = it's the var input, and false = it's the outcome output
+*/
+fn set_source_file(file_loc:String, input:bool){
+  
+}
